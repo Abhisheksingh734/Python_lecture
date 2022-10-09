bfb1985eb3ddab63b2bb2e0faf33d41c11c2f4c1
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-10 00:25:43 +0530

    string slicing methods

---
+++ stringSlicing.py
@@ -0,0 +1,12 @@
+
+
+string = "Abhishek Singh"
+        #  string slicing 
+print(string[0:len(string)])
+
+# string[3] = "r"  ---> does not work         #strings are immutable
+
+ #negative index
+
+print(string[::-1])  #reverese the string
+print(string[9:4])
