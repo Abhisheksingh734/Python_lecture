efb1246ebdb38eb68c71222b6e977c0d630af766
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-10 00:25:13 +0530

    these are functions of string

---
+++ stringFunctions.py
@@ -0,0 +1,28 @@
+Story= "Once upon a time there was a biy named Abhishek singh and he wanted to be software developer at Microsoft so he joined LPU..."
+
+# string functions
+
+print(len(Story)) #prints how the length of string
+
+
+#checks the ending letters and returns True/False
+print(Story.endswith("LPU..."))
+
+
+#to count the number of Occurence 
+print(Story.count("a"))
+
+#capitalized the first word of string
+print(Story.capitalize())
+
+#gives the index value 
+print(Story.find("Abhishek"))
+
+#to replace
+print(Story.replace("Microsoft","Google"))
+
+
+
+
+
+
