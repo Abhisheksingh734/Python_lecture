a11f16f4eec976c3b64812bc584bcb28f43ac9df
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-11 23:30:35 +0530

    making dictionary

---
+++ Dictionary.py/Dict_Syntax.py
@@ -0,0 +1,13 @@
+mydict = {
+    "Name": "Abhishek",
+    "Age": "19",
+    "Package": "80 LPA",
+    "anotherDict": {'Abhishek': 'Player'}
+}
+
+print(mydict["Name"])
+print(mydict["Age"])
+print(mydict["Package"])
+
+# Nested Dictionary
+print(mydict["anotherDict"]["Abhishek"])
