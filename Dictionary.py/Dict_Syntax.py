abd797cf25d300dcd4c062519e8c8a8b5c33081d
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-11 23:45:44 +0530

    Dictionary methods

--- Dictionary.py/Dict_Syntax.py
+++ Dictionary.py/Dict_Syntax.py
@@ -2,12 +2,19 @@ mydict = {
     "Name": "Abhishek",
     "Age": "19",
     "Package": "80 LPA",
-    "anotherDict": {'Abhishek': 'Player'}
+    "anotherDict": {'Abhishek': 'Player'},
+    1:2
 }
 
 print(mydict["Name"])
 print(mydict["Age"])
 print(mydict["Package"])
+print(mydict[1])
 
 # Nested Dictionary
 print(mydict["anotherDict"]["Abhishek"])
+
+print(list(mydict)) #print KEYS of dictionaries
+print(list(mydict.values())) #print values of Dictionary
+print((mydict.keys()))
+print(mydict.items())
---
+++ Dictionary.py/updateDict.py
@@ -0,0 +1,17 @@
+mydict = {
+    "Name": "Abhishek",
+    "Age": "19",
+    "Package": "80 LPA",
+    "anotherDict": {'Abhishek': 'Player'},
+    1:2
+}
+
+print(mydict)
+
+updateDict={"Good":"Friend",
+"D": "Friend"
+
+
+}                                   #what you eant to update
+mydict.update(updateDict)   #update function
+print(mydict)
