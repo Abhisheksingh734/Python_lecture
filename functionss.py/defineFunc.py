d44812cb2f75ab53f891bad271c7e022e4c869c3
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-23 20:39:28 +0530

    define a function

---
+++ functionss.py/defineFunc.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print(f"Hello {name}")
+
+
+
+
+greet("abhishek singh")
+
+
+
+
+def mysum(a,b):
+    return a+b 
+
+a=mysum(2,4)
+print(a,"It returns value")
+
+print(mysum(4,6))
