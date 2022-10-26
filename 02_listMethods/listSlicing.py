c166dd9c0f58241d9c130d3baf70cfd4d7906686
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-10-26 12:51:09 +0530

    List methids

---
+++ 02_listMethods/listSlicing.py
@@ -0,0 +1,6 @@
+# Slicing a list 
+list=[1,2,3,4,5,6,7,8,9,4]
+print(list[0:4])
+
+list.sort() 
+print(list)
---
+++ 02_listMethods/popAndRemove.py
@@ -0,0 +1,8 @@
+list =[2,3,4,5,6,7,8]
+
+a=list.pop() #removes element for end of the list and return the removed value
+print(a)         #removed element
+print(list)        #list
+
+a=max(list)
+print(a)
