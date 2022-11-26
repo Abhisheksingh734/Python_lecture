700ccd0d9508d80ecc45b08ac52392fa868155be
Abhishek singh <abhisheksinghasr734@gmail.com>
2022-11-26 23:45:29 +0530

    Pattern

--- 07_loop/pattern3.py
+++ 07_loop/pattern3.py
@@ -3,12 +3,37 @@
 #                   * *
 #                   ***
 
-n=int(input("enter rows: "))
+# n=int(input("enter rows: "))
 
-for i in range(n):
-    print("*",end="")
-    if i==(n//2):
-        print(" ",end="")
-    else:
-        print("*",end="")
-    print("*")
+# for i in range(n):
+#     print("*",end="")
+#     if i==(n//2):
+#         print(" ",end="")
+#     else:
+#         print("*",end="")
+#     print("*")
+
+
+
+
+
+
+# Increasing triangle pattern
+
+# rows=int(input())
+# for i in range(rows):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+
+
+
+
+
+#Decreasing triangle pattern
+rows=int(input())
+for i in range(rows):
+    for j in range(rows-i):
+        print("*",end=" ")
+    print()
+    
