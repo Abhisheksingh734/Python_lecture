bf2691d6e38b877da580d28869e0b5c3bd271a55
Abhishek <abhisheksinghasr734@gmail.com>
2022-12-22 22:28:34 +0530

    zip function

--- zipfunc.py
+++ zipfunc.py
@@ -2,10 +2,10 @@ l1=[1,2,3]
 l2=["a","b","c"]
 
 new=list(zip(l1,l2))
-print(new)
 
-# z1,z2=zip(*new)
-# print(z1,z2)
+z1,z2=zip(*new)
+print(z1,z2)
+print(new)
 # # print(list(map(list,zip(*new))))
 
 # data=[34,5,67,78,3546,34645,6345,65,54,6,5436,56]
