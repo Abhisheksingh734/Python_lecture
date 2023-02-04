20972a4d205f3beb848668d5c8e33bf89541081e
Abhishek <abhisheksinghasr734@gmail.com>
2023-02-04 10:52:26 +0530

    marvelous number

--- ENDTerm/marvelousNum.py
+++ ENDTerm/marvelousNum.py
@@ -1,7 +1,6 @@
-def ``
-
-count =0
-for i in range(r1,r2+1):
-    if(i-int(str(i)[::-1]))%k==0:
-        count+=1
-print(count) 
+def marvelous(r1,r2,k):
+    count =0
+    for i in range(r1,r2+1):
+        if(i+int(str(i)[::-1]))%k==0:
+            count+=1
+    print(count) 
